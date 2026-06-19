03e804be4929dff10e1a669a217f8e31760e9496
pimdao4321-art <pimdao4321@gmail.com>
2026-06-19 09:13:46 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
-document. addEventListener('DOMContentLoaded', function () {
-console.log('Portfolio ของ phimdao พร้อมแล้ว! ') ;
-}) ;
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portfolio ของ PHIMDAO พร้อมแล้ว!');
+});
